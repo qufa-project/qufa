d174171b00ab8398b75e2b5f170e0362e02e2fce
jihoonkim97 <meaningkim@naver.com>
2021-07-22 17:01:13 +0900

    modify fields of DateProfile Model

--- dataQuality/hsu/profiler/src/main/java/com/QuFa/profiler/model/profile/DateProfile.java
+++ dataQuality/hsu/profiler/src/main/java/com/QuFa/profiler/model/profile/DateProfile.java
@@ -1,4 +1,19 @@
 package com.QuFa.profiler.model.profile;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+@Setter
 public class DateProfile {
+    String highest_date;
+    String lowest_date;
+    String mean_date;
+    String median_date;
+    String percentile_25th;
+    String percentile_75th;
+    Map<Object, Object> month_distribution;
+    Map<Object, Object> year_distribution;
 }
