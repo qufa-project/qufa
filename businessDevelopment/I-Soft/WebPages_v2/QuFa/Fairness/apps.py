c28addccaa53dc0d752c7c6007c63c8ea6fc83e4
redzoo <zoored@daum.net>
2021-12-27 17:34:29 +0900

    텐서 플로 하드웨어 환경 설정 코드 변경

--- businessDevelopment/I-Soft/WebPages_v2/QuFa/Fairness/apps.py
+++ businessDevelopment/I-Soft/WebPages_v2/QuFa/Fairness/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
 
+import os
+import tensorflow as tf
 
 class FairnessConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Fairness'
+    
+    os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'
+    physical_devices = tf.config.list_physical_devices('GPU') 
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
