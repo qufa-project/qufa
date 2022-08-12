0c9aa97b343b5f0ab81e8e63421921bfb889290e
im-shung <seoyoung1113@gmail.com>
2022-08-12 11:42:43 +0900

    :art: 에러 표현 형식 수정

--- dataQuality/hsu/profiler/src/main/java/com/QuFa/profiler/controller/exception/ErrorCode.java
+++ dataQuality/hsu/profiler/src/main/java/com/QuFa/profiler/controller/exception/ErrorCode.java
@@ -9,17 +9,16 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     /* 400 BAD_REQUEST : 잘못된 요청 */
-    BAD_JSON_REQUEST(HttpStatus.BAD_REQUEST,"400 Bad Json Request"),
-
-    /* 401 UNAUTHORIZED : 잘못된 요청 */
+    BAD_JSON_REQUEST(HttpStatus.BAD_REQUEST,"400"),
 
     /* 404 NOT_FOUND : Resource 를 찾을 수 없음 */
-    FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "404 File Not Found"),
+    FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "404"),
 
-    REQUEST_DATA_MALFORMED(HttpStatus.LENGTH_REQUIRED,"411 Request Data Malformed"),
+    /* 411 LENGTH_REQUIRED : Request Data Malformed */
+    REQUEST_DATA_MALFORMED(HttpStatus.LENGTH_REQUIRED,"411"),
 
     /* 500 SERVER_ERROR */
-    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"500 Internal Error");
+    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"500");
 
     private final HttpStatus httpStatus;
     private final String detail;
