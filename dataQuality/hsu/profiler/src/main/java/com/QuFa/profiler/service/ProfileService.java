31f137dff1818807a9ce046065c6f29702ba60ac
ssw <skowko6750@naver.com>
2022-10-18 15:13:29 +0900

    정수, 소수 판별 로직 수정

--- dataQuality/hsu/profiler/src/main/java/com/QuFa/profiler/service/ProfileService.java
+++ dataQuality/hsu/profiler/src/main/java/com/QuFa/profiler/service/ProfileService.java
@@ -648,7 +648,7 @@ public class ProfileService {
         }
 
         boolean isDouble = false;
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < keyArray.length; i++) {
             if (keyArray[i].toString().contains(".")) {
                 isDouble = true;
                 break;
